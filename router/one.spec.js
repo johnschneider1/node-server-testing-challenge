5943ce036d581fa136faeeceb36423c67b9b248e
johnschneider1 <johnlawschneider@gmail.com>
2019-09-19 17:44:29 -0500

    tests up and running api deployed

--- router/one.spec.js
+++ router/one.spec.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const server = require("../api/server.js");
 const db = require("../data/dbConfig.js");
+const Users = require("../router/oneModel.js");
 
 describe("oneRouter.js", () => {
   describe("POSt /api/users", () => {
@@ -11,13 +12,37 @@ describe("oneRouter.js", () => {
           expect(res.status).toBe(200);
         });
     });
-    it("successful delete", () => {
-      return request(server)
-        .delete("/api/users/")
-        .then(res => {
-          expect(res.status).toBe(200);
-          expect(res.body).toBe(1);
-        });
+    describe("add()", () => {
+      beforeEach(async () => {
+        await db("users").truncate();
+      });
+
+      it("should add users to the db", async () => {
+        await Users.add({ username: "bill", password: "ted" });
+
+        let users = await db("users");
+
+        expect(users).toHaveLength(1);
+      });
     });
   });
 });
+
+// describe("add()", () => {
+//     it("should add users to the db", async () => {
+//       await Users.add({ username: "bill" });
+
+//       let users = await db("users");
+
+//       expect(users).toHaveLength(2);
+//     });
+//   });
+
+//   it("successful delete", () => {
+//     return request(server)
+//       .delete("/api/users/")
+//       .then(res => {
+//         expect(res.status).toBe(200);
+//         expect(res.body).toBe(1);
+//       });
+//   });
